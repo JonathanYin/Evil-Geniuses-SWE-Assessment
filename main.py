bfe155917cdc4c61f05b22484632d512e775eeac
JonathanYin <joyin@ucsd.edu>
2023-05-29 22:41:47 -0700

    Finish part 1

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# main.py
 from process_game_state import ProcessGameState
+from shapely.geometry import Point, Polygon
 
-# Initialize the class with your parquet file
-game_state = ProcessGameState("./data/game_state_frame_data.parquet")
+file_path = './data/game_state_frame_data.parquet'
+game_state = ProcessGameState(file_path)
 
-# Check if each row falls within the boundary
-boundary_coordinates = {'x': [-2806, -1565], 'y': [250, 1233], 'z': [285, 421]}
-boundary_data = game_state.in_boundary(boundary_coordinates)
+# Define boundary
+boundary_vertices = [
+    [-1735, 250],  # 13
+    [-2024, 398],  # 14
+    [-2806, 742],  # 15
+    [-2472, 1233],  # 16
+    [-1565, 580]  # 17
+]
 
-# Extract weapon classes from inventory
-weapon_data = game_state.extract_weapon_classes()
+boundary_polygon = Polygon(boundary_vertices)
+z_boundary = [285, 421]
 
-# Print to check
-print(boundary_data.head())
-print(weapon_data.head())
+# Check if each row is within boundary
+game_state.data['in_boundary'] = game_state.data.apply(
+    lambda row: boundary_polygon.contains(Point(row['x'], row['y'])) and z_boundary[0] <= row['z'] <= z_boundary[1], axis=1)
+
+
+def boundary_fraction(game_state):
+    # Filter out the actions of Team2 when they are playing T-side.
+    team2_actions = game_state.data.loc[(
+        game_state.data['team'] == 'Team2') & (game_state.data['side'] == 'T')].copy()
+
+    # Calculate the fraction of Team2's T-side actions within the boundary.
+    fraction_in_boundary = team2_actions['in_boundary'].sum(
+    ) / len(team2_actions)
+
+    print("Fraction of Team2's T-side actions within the boundary:",
+          fraction_in_boundary)
+
+    # Unique entry instances
+    team2_actions.loc[:, 'entry_instance'] = (
+        team2_actions['in_boundary'].shift() != team2_actions['in_boundary'])
+    unique_entry_instances = team2_actions['entry_instance'].sum()
+
+    # Number of entries in the boundary
+    entries_in_boundary = team2_actions['in_boundary'].sum()
+
+    # Time spent in boundary
+    time_in_boundary = entries_in_boundary * (1 / 128)
+
+    print(f"Team2 entered the boundary {entries_in_boundary} times.")
+    print(
+        f"Team2 spent a total of {time_in_boundary} seconds within the boundary.")
+    print(
+        f"Team2 had {unique_entry_instances} unique instances of entering the boundary.")
+
+
+boundary_fraction(game_state)
+
+# Extract weapon classes
+weapon_classes_data = game_state.extract_weapon_classes()
+print(weapon_classes_data)
--- process_game_state.py
+++ process_game_state.py
@@ -1,24 +1,53 @@
-# process_game_state.py
-import pandas as pd
+from matplotlib.path import Path
 import json
+import pandas as pd
 
 
 class ProcessGameState:
-    def __init__(self, data_path):
-        self.data = pd.read_parquet(data_path)
+    def __init__(self, file_path):
+        self.data = pd.read_parquet(file_path)
 
-    def in_boundary(self, coordinates):
-        x_min, x_max = coordinates['x']
-        y_min, y_max = coordinates['y']
-        z_min, z_max = coordinates['z']
+    def is_in_boundary(self, boundary):
+        # Construct a polygon from boundary points
+        polygon = Path([(boundary['13']), (boundary['15']),
+                        (boundary['16']), (boundary['17'])])
 
-        self.data['in_boundary'] = (self.data['x'].between(x_min, x_max) &
-                                    self.data['y'].between(y_min, y_max) &
-                                    self.data['z'].between(z_min, z_max))
+        # Get x, y coordinates
+        x = self.data['x']
+        y = self.data['y']
+
+        # Check if each pair of x, y coordinates is within the boundary
+        self.data['in_boundary'] = polygon.contains_points(list(zip(x, y)))
         return self.data
 
     def extract_weapon_classes(self):
-        self.data['inventory'] = self.data['inventory'].apply(json.loads)
-        self.data['weapon_classes'] = self.data['inventory'].apply(
-            lambda x: x.get('weapon', None))
+        self.data['inventory'] = self.data['inventory'].apply(
+            lambda x: json.loads(x) if isinstance(x, str) else x)
         return self.data
+
+
+def boundary_analysis(game_state, tick_rate):
+    # Filter out the actions of Team2 when they are playing T-side.
+    team2_actions = game_state.data.loc[(game_state.data['team'] == 'Team2') &
+                                        (game_state.data['side'] == 'T')]
+
+    # Filter out actions within the boundary
+    team2_in_boundary = team2_actions.loc[team2_actions['in_boundary'] == True]
+
+    # Calculate the total ticks and total time spent in the boundary
+    total_ticks_in_boundary = len(team2_in_boundary)
+    total_time_in_boundary = total_ticks_in_boundary / \
+        tick_rate  # Convert ticks to seconds
+
+    # Calculate the number of unique boundary entry instances
+    # By checking when 'in_boundary' changes from False to True
+    team2_actions['entry_instance'] = (
+        team2_actions['in_boundary'].shift() != team2_actions['in_boundary'])
+    unique_entries = len(team2_actions.loc[(team2_actions['in_boundary'] == True) &
+                                           (team2_actions['entry_instance'] == True)])
+
+    print(f"Team2 entered the boundary {total_ticks_in_boundary} times.")
+    print(
+        f"Team2 spent a total of {total_time_in_boundary} seconds within the boundary.")
+    print(
+        f"Team2 had {unique_entries} unique instances of entering the boundary.")
